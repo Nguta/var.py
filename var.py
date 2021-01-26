07369eaa37a47e8be2699da687082cd001e47036
Nguta <miriamnguta949@gmail.com>
2021-01-26 13:31:56 +0300

    first commit

---
+++ var.py
@@ -0,0 +1,15 @@
+myfriend = 4
+print(myfriend)
+4
+a,v =(72,54)
+print(a,v)
+print (v + a)
+print (v*a)
+print (myfriend + v)
+green = 88
+blue = 11
+print(green /blue)
+print(myfriend * blue)
+print (green + v)
+
+
